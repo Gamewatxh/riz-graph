dcf88196061d6040c2eb62e1638c727cb0de4828
Gamewatxh <124492172+Gamewatxh@users.noreply.github.com>
2024-03-14 11:01:38 +0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -34,7 +34,7 @@ export default function App({ Component, pageProps }) {
             ) : (
               <>
               {/*  <Body /> */}
-               {/* <Component {...pageProps} /> */}
+              <Component {...pageProps} /> 
               </>
             )}
           </div>
