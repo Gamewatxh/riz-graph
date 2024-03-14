0ec92a5732d974a6a115dee9fd189c14d0b1b755
Gamewatxh <124492172+Gamewatxh@users.noreply.github.com>
2024-03-14 11:34:39 +0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -38,6 +38,7 @@ export default function App({ Component, pageProps }) {
                <landing /> 
               <Component {...pageProps} /> 
               </>
+               <landing /> 
             )}
           </div>
         </NotificationProvider>
