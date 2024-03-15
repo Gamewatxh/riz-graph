4923f7b088a34e601f595a05fa5907834ec2c89a
Gamewatxh <124492172+Gamewatxh@users.noreply.github.com>
2024-03-15 05:39:44 -0700

    Update NavBar.jsx

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,20 +1,42 @@
 import { ConnectButton } from "@web3uikit/web3";
 import { useRouter } from "next/router";
 
-export default function NavBar () {
-
+export default function NavBar() {
     const router = useRouter();
-    const handleClick = () => {
+
+    const handleClickHome = () => {
+        router.push('/LandingPage');
+    };
+
+    const handleClickMarketplace = () => {
         router.push('/Marketplace');
     };
+
+    const handleClickRegisterInChain = () => {
+        router.push('/Body');
+    };
+
+    const handleClickBlog = () => {
+        router.push('/Blog');
+    };
+
     return (
         <nav className="nav--bar">
             <h1>
-                Pro-Block
+                Top Talent Chain
             </h1>
             <div className="right--side">
-                <label onClick={handleClick}>
+                <label onClick={handleClickHome}>
+                    Home
+                </label>
+                <label onClick={handleClickMarketplace}>
                     Marketplace
+                </label>
+                <label onClick={handleClickRegisterInChain}>
+                    Register in Chain
+                </label>
+                <label onClick={handleClickBlog}>
+                    Blog
                 </label>
                 <ConnectButton chainId={11155111} />
             </div>
